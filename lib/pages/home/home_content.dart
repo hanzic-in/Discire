7331e131f03bd51902ffecd864efcaae6c644c27
Han <rizkyupit40@gmail.com>
2026-04-23 10:05:08 +0700

    Add HomeContent widget with layout structure

--- lib/pages/home/home_content.dart
+++ lib/pages/home/home_content.dart
@@ -1,1 +1,23 @@
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      usePadding: false, 
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const HomeHeader(),
+            const HomeSearchBar(),
+            const HomeChips(),
+            const NearbySection(),
+            const PostSection(),
+            const SizedBox(height: 120),
+          ],
+        ),
+      ),
+    );
+  }
+}
