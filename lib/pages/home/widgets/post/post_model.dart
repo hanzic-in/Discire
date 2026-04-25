c976bd49550c97219010d82f732f363f1942b639
Han <rizkyupit40@gmail.com>
2026-04-25 14:51:52 +0700

    Add Post model with PostType enumeration

---
+++ lib/pages/home/widgets/post/post_model.dart
@@ -0,0 +1,19 @@
+enum PostType {
+  text,
+  image,
+  article,
+}
+
+class Post {
+  final PostType type;
+  final String content;
+  final List<String>? images;
+  final String? title;
+
+  Post({
+    required this.type,
+    required this.content,
+    this.images,
+    this.title,
+  });
+}
