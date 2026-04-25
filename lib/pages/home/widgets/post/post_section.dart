631d67463355f3c758d722e89b9c62e8006bb198
Han <rizkyupit40@gmail.com>
2026-04-25 14:48:06 +0700

    Implement PostSection widget with dummy content

--- lib/pages/home/widgets/post/post_section.dart
+++ lib/pages/home/widgets/post/post_section.dart
@@ -1,1 +1,40 @@
-dk
+import 'package:flutter/material.dart';
+
+class PostSection extends StatelessWidget {
+  const PostSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.fromLTRB(16, 25, 16, 15),
+          child: Text(
+            "What's Happening",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ),
+
+        _dummyPost(),
+
+        const SizedBox(height: 100),
+      ],
+    );
+  }
+
+  Widget _dummyPost() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const Text("Placeholder"),
+    );
+  }
+}
