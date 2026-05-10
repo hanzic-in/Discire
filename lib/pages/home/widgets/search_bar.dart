b05dec561b57894f94d3726e6700a3928bf2b69e
Han <rizkyupit40@gmail.com>
2026-05-10 13:50:03 +0700

    Enhance search bar appearance with opacity and shadows
    Updated the search bar's background color to use an opacity effect and added box shadow for better visual depth.

--- lib/pages/home/widgets/search_bar.dart
+++ lib/pages/home/widgets/search_bar.dart
@@ -11,9 +11,22 @@ class HomeSearchBar extends StatelessWidget {
         height: 55,
         padding: const EdgeInsets.symmetric(horizontal: 16),
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-        ),
+  color: Colors.white.withOpacity(0.9),
+  borderRadius: BorderRadius.circular(30),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.white.withOpacity(0.45),
+      blurRadius: 24,
+      spreadRadius: 2,
+      offset: const Offset(0, 6),
+    ),
+    BoxShadow(
+      color: Colors.black.withOpacity(0.04),
+      blurRadius: 16,
+      offset: const Offset(0, 8),
+    ),
+  ],
+),
         child: const Row(
           children: [
             Icon(Icons.search, color: Colors.grey),
